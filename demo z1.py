1535bb3324d4b865ccc65789a139b35e64bc5389
123YoLo <79315752+123YoLo@users.noreply.github.com>
2021-02-28 08:15:00 +0800

    用Python编写猜拳游戏
    自己编写的python代码

---
+++ demo z1.py
@@ -0,0 +1,26 @@
+# -*-coding = utf-8 -*-
+# @Time : 2021/2/28 8:08
+# @Author :123YoLo
+# @File : demo z1.py
+# @Software: PyCharm
+#我自己做的答案
+'''
+print("请输入：剪刀（0）、石头（1）、布（2）:")
+x=input()
+print(type(x))
+a=int(x)
+import random
+y=random.randint(0,2)
+if a<y:
+    if y-a == 1:
+        print("哈哈，你输了")
+    else:
+        print("你赢了")
+elif a>y:
+    if a-y == 0 :
+        print("你赢了")
+    else:
+        print("哈哈，你输了")
+else:
+    print("平手")
+'''
